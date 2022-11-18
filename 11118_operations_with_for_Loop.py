38497d541e948f47ed83329a535af77b39f75616
hakan <hakankan0117@gmail.com>
2022-11-18 03:01:49 +0300

    modified 11118*.py

--- 11118_operations_with_for_Loop.py
+++ 11118_operations_with_for_Loop.py
@@ -213,3 +213,9 @@
 #     print(" " * (n - i - 1) + "* " * (i + 1))
 # for k in range(n-1, 0, -1):
 #     print(" " * (n - k) + "* " * k)
+
+n = 5
+for i in range(n):
+    print(" " * (n - i - 1) + "* " * (i + 1))
+for k in range(n-1, 0, -1):
+    print(" " * (n - k) + "* " * k)
