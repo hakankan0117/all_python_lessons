eb17fe816a98047c4804e836da8822d1146b8a03
hakan <hakankan0117@gmail.com>
2022-11-19 15:16:00 +0300

    modified all files

---
+++ 09_errors._exceptions.py
@@ -0,0 +1,80 @@
+# print("Don't say I never make a mistake"
+
+# print(""" don' sya i never make a mistake")
+
+# pirnt(" don not say never i make a mistake")
+
+# x = ["1", "2", "3"]
+# y = ["USA", "Japan", "Spain"]
+# for i in y:
+#     for j in X:
+#         print(type([tuple(i+j)]))
+
+a = 0
+b = 2
+print(a // b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# while True:
+#     no_one = int(input("The first number please : "))
+#     no_two = int(input("The second number please : "))
+#     try:
+#         division = no_one / no_two  # normal part of the program
+#     except ZeroDivisionError:
+#         print("You can't divide by zero! Try again.")  # executes when division by zero
+#     else:
+#         print("The result of the division is : ", division)  # executes if there is no exception
+#     finally:
+#         print("Thanks for using our mini divison calculator! Come again!")
+#         break  # exits the while loop
+
+# while True:
+#     no_one = int(input("The first number please : "))
+#     no_two = int(input("The second number please : "))
+#     try:
+#         division = no_one / no_two
+#         print("The result of the division is : ", division)
+#         break
+#     except Exception as e:
+#         print("Something went wrong...Try again.")
+#         print("Probably it is because of '{}' error".format(e))
+#         break
