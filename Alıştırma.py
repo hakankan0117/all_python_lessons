70af4cf34a17886f3033e2f558ef7c8de06ffac3
hakan <hakankan0117@gmail.com>
2022-12-12 01:50:57 +0300

    alıştırma dosyası oluşturuldu

---
+++ Alıştırma.py
@@ -0,0 +1,13 @@
+
+value = input("lütfen bir cümle giriniz : ")
+
+a = ("a", "b", "c","ç", "d", "e", "f", "g","ğ", "h", "ı", "i", "j", "k", "l", "m",
+     "n", "o", "ö", "q", "p", "r", "s", "ş", "t", "u", "ü", "v", "w", "y", "z")
+
+dizi = []
+
+for i in value :
+    if not (i in dizi) and i != " "  and i in a:
+        dizi.append(i)
+
+print(dizi)
